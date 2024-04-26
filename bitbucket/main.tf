7727ceddc1020892d73160ecf8395fb6802963df
Simon Lynch <simon.lynch@hashicorp.com>
2024-04-26 10:16:50 +1000

    add bitbucket

--- bitbucket/main.tf
+++ bitbucket/main.tf
@@ -1,7 +1,22 @@
-resource "bitbucket_project" "example" {
-  workspace   = "mgl-network-poc"
-  name        = "Example Project"
-  key         = "EXP"
-  description = "An example project"
-  is_private  = true
+resource "bitbucket_project" "this" {
+  for_each = var.bitbucket_projects
+
+  workspace   = each.value.workspace
+  name        = each.value.name
+  key         = each.value.key
+  description = each.value.description
+  is_private  = each.value.is_private
+}
+
+
+resource "bitbucket_repository" "this" {
+  for_each = var.bitbucket_repositories
+  
+  name = each.value.name
+  project_key = bitbucket_project.this[each.value.project_key].key
+  workspace = each.value.workspace
+  enable_pipelines = each.value.enable_pipelines
+  language = each.value.language
+  is_private = each.value.is_private
+  
 }
--- bitbucket/variables.tf
+++ bitbucket/variables.tf
@@ -6,3 +6,42 @@
   default     = null
 } */
 
+variable "bitbucket_projects" {
+  type = map(object({
+    name = string
+    key = string
+    workspace = string
+    description = optional(string)
+    is_private = bool
+  }))
+  default = {
+    "TestProject" = {
+      is_private = false
+      key = "TST"
+      name = "TestProject"
+      workspace = "mgl-network-poc"
+    }
+  }
+}
+
+variable "bitbucket_repositories" {
+  type = map(object({
+    name = string
+    project_key = string
+    workspace = string
+    enable_pipelines = optional(bool)
+    language = optional(string)
+    is_private = optional(bool)
+  }))
+  default = {
+    "someRepo2" = {
+      name = "testrepo2"
+      project_key= "TestProject"
+      workspace = "mgl-network-poc"
+      is_private = false
+      enable_pipelines = true
+      language = "other"
+    }
+  }
+}
+
