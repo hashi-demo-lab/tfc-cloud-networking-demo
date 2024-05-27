04a16349db1aabf39c1fd573c8d3e8e0e80a59cf
Simon Lynch <simon.lynch@hashicorp.com>
2024-05-27 12:52:58 +1000

    update

--- terraform-gcp-oidc-dynamic-creds/gcp.tf
+++ terraform-gcp-oidc-dynamic-creds/gcp.tf
@@ -19,7 +19,6 @@ data "google_project" "project" {
 resource "google_project_service" "services" {
   count   = length(var.gcp_service_list)
   service = var.gcp_service_list[count.index]
-  disable_dependent_services = true
 }
 
 # Creates a workload identity pool to house a workload identity
